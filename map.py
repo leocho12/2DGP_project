ea05657c1fe733f2d947a2abe74f6fc5ed1f9c29
leocho12 <hjc021212@gmail.com>
2025-10-23 20:50:57 +0900

    add grass to map.py

---
+++ map.py
@@ -0,0 +1,8 @@
+from pico2d import load_image
+
+class Map:
+    def __init__(self):
+        self.image = load_image('Theme_02_L130_Hill_01.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
