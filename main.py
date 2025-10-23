b49f8d939db2e36ed8d4ca29dd5be12c6998c55c
leocho12 <hjc021212@gmail.com>
2025-10-23 20:59:02 +0900

    init main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 from pico2d import*
+from map import Map
+
+def handle_events():
+    pass
+
+def reset_world():
+    pass
+
+def update_world():
+    pass
+
+def render_world():
+    pass
+
+running=True
+
+open_canvas()
+reset_world()
+#game main loop
+while running:
+    handle_events()
+    update_world()
+    clear_canvas()
+    render_world()
+    delay(0.01)
+#finalization code
+close_canvas()
