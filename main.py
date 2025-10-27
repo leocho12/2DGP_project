1e04c33d59b7fb93ffb47096439101a550b5a30a
leocho12 <hjc021212@gmail.com>
2025-10-27 14:55:50 +0900

    main.py setting

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from pico2d import*
+
+
 from map import Map
 from duck import Duck
 
 
 def handle_events():
+    global running
+
+    event_list=get_events()
+    for event in event_list:
+        if event.type==SDL_QUIT:
+            running=False
+        elif event.type==SDL_KEYDOWN and event.key==SDLK_ESCAPE:
+            running=False
+        else:
+            duck.handle_event(event)
     pass
 
 def reset_world():
