67d705300b653573c812068d0904299f872d7ec0
leocho12 <hjc021212@gmail.com>
2025-11-05 12:08:04 +0900

    상태 클래스 생성

--- duck.py
+++ duck.py
@@ -2,14 +2,14 @@ from pico2d import *
 import game_world
 from state_machine import StateMachine
 
-# zombie Run Speed
+# bird Run Speed
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
 RUN_SPEED_KMPH = 10.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-# zombie Action Speed
+# bird Action Speed
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 10.0
