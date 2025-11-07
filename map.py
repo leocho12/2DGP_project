3387121ff6d11a0f327b261203ca5ed90daf4094
leocho12 <hjc021212@gmail.com>
2025-11-07 17:43:01 +0900

    배경 이미지 위치,크기 수정

--- map.py
+++ map.py
@@ -1,11 +1,17 @@
-from pico2d import load_image
+from pico2d import load_image, get_canvas_width, get_canvas_height
+
 
 class Map:
     def __init__(self):
         self.image = load_image('Theme_02_L130_Hill_01.png')
+        self.cw=get_canvas_width()
+        self.ch=get_canvas_height()
+        self.w=self.cw
+        self.h=200
+
 
     def draw(self):
-        self.image.draw(400, 300)
+        self.image.draw(self.cw//2, self.h//2, self.w, self.h)
 
     def update(self):
         pass
