44f2c773e13e928464c915bfa888827baca76061
leocho12 <hjc021212@gmail.com>
2025-11-08 20:42:02 +0900

    발사시 조준범 약간 줄어드는 이펙트 추가

--- gun.py
+++ gun.py
@@ -12,6 +12,7 @@ class Gun:
     image = None
 
     def __init__(self,world=None):
+        self.recoil_scale =0.9  # 반동 시 축소 비율
         self.world = world
         self.x, self.y=400,60
         self.frame=0
@@ -39,20 +40,33 @@ class Gun:
                 self.fire()
 
     def draw(self):
-        offset_y=6 if self.recoil_timer > 0.0 else 0
-        if Gun.image:
-            Gun.image.draw(self.x, self.y)
+        # 반동이 남아있으면 스케일을 작게 적용하고 시간이 지나며 원래 크기로 복원
+        if self.recoil_timer > 0.0 and self.recoil_duration > 0.0:
+            t = self.recoil_timer / self.recoil_duration  # 0..1
+            # 남은 시간 비율에 따라 스케일을 보간 (발사직후 작고 천천히 1.0으로)
+            scale = self.recoil_scale + (1.0 - self.recoil_scale) * (1.0 - t)
         else:
-            w, h = 40, 20
-            draw_rectangle(self.x - w // 2, self.y - h // 2, self.x + w // 2, self.y + h // 2)
+            scale = 1.0
 
+        if Gun.image:
+            iw = getattr(Gun.image, 'w', 40)
+            ih = getattr(Gun.image, 'h', 20)
+            Gun.image.draw(self.x, self.y, iw * scale, ih * scale)
+        else:
+            base_w, base_h = 40, 20
+            w = base_w * scale
+            h = base_h * scale
+            draw_rectangle(self.x - w // 2, self.y - h // 2, self.x + w // 2, self.y + h // 2)
 
     def fire(self):
         self.recoil_timer = self.recoil_duration
         # 총알 발사 효과음 재생 (효과음 파일이 있다고 가정)
 
         # 충돌 검사
-        duck_layer = game_world.world[1]  # 오리들이 있는 레이어
+        try:
+            duck_layer = game_world.world[1]
+        except Exception:
+            duck_layer = []
 
         for i in list(duck_layer):
             if hasattr(i,'get_bb'):
