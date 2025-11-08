7484ad7ddc3e59bc4d8ed80a4480e20ae7e6cb8a
leocho12 <hjc021212@gmail.com>
2025-11-08 19:45:41 +0900

    gun.py statemachine사용하지 않도록 수정

--- gun.py
+++ gun.py
@@ -2,7 +2,7 @@ from pico2d import *
 from sdl2 import *
 
 import game_world
-from state_machine import StateMachine
+import game_framework
 
 class Gun:
     image = None
@@ -10,29 +10,30 @@ class Gun:
     def __init__(self,world):
         self.x, self.y=400,60
         self.frame=0
-        self.image=load_image('duck.png')
-
-        self.IDLE=Idle(self)
-        self.SHOOT=Shoot(self)
-
-        self.state_machine = StateMachine(
-            self.IDLE,
-            {
-                self.IDLE:{},
-                self.SHOOT:{}
-            }
-        )
+        if Gun.image is None:
+            try:
+                self.image=load_image('gun.png')
 
     def update(self):
-        self.state_machine.update()
-
-    def handle_event(self, event):
-        self.state_machine.handle_state_event(('INPUT', event))
         pass
 
-    def draw(self):
-        self.state_machine.draw()
+    def handle_event(self, event):
+        if event.type == SDL_MOUSEMOTION:
+            self.x = event.x
+            self.y = get_canvas_height() - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if getattr(event, 'button', None) == 1:
+                self.fire()
 
-    def fire(self):
-        bullet=Bullet(self.x,self.y)
-        game_world.add_objects(bullet,1)
+    def draw(self):
+    if Gun.image:
+        Gun.image.draw(self.x, self.y)
+    else:
+        w, h = 40, 20
+        draw_rectangle(self.x - w // 2, self.y - h // 2, self.x + w // 2, self.y + h // 2)
+
+
+def fire(self):
+    from gun import Bullet as _Bullet  # 같은 파일 안에 있을 경우 안전 호출(혹은 위에서 직접 사용)
+    bullet = _Bullet(self.x, self.y + 20)
+    game_world.add_object(bullet, 2)
