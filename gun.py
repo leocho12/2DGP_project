4073216f74142d0c068cd11b46385c31c65650fd
leocho12 <hjc021212@gmail.com>
2025-11-08 20:21:52 +0900

    총 반동,발사 작성

--- gun.py
+++ gun.py
@@ -4,18 +4,26 @@ from sdl2 import *
 import game_world
 import game_framework
 
+def _point_in_bb(px, py, bb):
+    l, b, r, t = bb
+    return l <= px <= r and b <= py <= t
+
 class Gun:
     image = None
 
     def __init__(self,world):
         self.x, self.y=400,60
         self.frame=0
-        if Gun.image is None:
-            try:
-                self.image=load_image('gun.png')
+        self.recoil_timer=0.0
+        self.recoil_duration=0.12
+        self.world=world
+        self.image=load_image('gun.png')
 
     def update(self):
-        pass
+        if self.recoil_timer > 0.0:
+            self.recoil_timer -= game_framework.frame_time
+            if self.recoil_timer < 0.0:
+                self.recoil_timer = 0.0
 
     def handle_event(self, event):
         if event.type == SDL_MOUSEMOTION:
@@ -26,14 +34,24 @@ class Gun:
                 self.fire()
 
     def draw(self):
-    if Gun.image:
-        Gun.image.draw(self.x, self.y)
-    else:
-        w, h = 40, 20
-        draw_rectangle(self.x - w // 2, self.y - h // 2, self.x + w // 2, self.y + h // 2)
+        offset_y=6 if self.recoil_timer > 0.0 else 0
+        if Gun.image:
+            Gun.image.draw(self.x, self.y)
+        else:
+            w, h = 40, 20
+            draw_rectangle(self.x - w // 2, self.y - h // 2, self.x + w // 2, self.y + h // 2)
 
 
 def fire(self):
-    from gun import Bullet as _Bullet  # 같은 파일 안에 있을 경우 안전 호출(혹은 위에서 직접 사용)
-    bullet = _Bullet(self.x, self.y + 20)
-    game_world.add_object(bullet, 2)
+    self.recoil_timer = self.recoil_duration
+    # 총알 발사 효과음 재생 (효과음 파일이 있다고 가정)
+
+    # 충돌 검사
+    duck_layer = game_world.world[1]  # 오리들이 있는 레이어
+
+    for i in list(duck_layer):
+        if hasattr(i,'get_bb'):
+            bb=i.get_bb()
+            if _point_in_bb(self.x, self.y, bb):
+                game_world.remove_object(i)
+                break  # 한 번에 하나의 오리만 맞출 수 있도록
