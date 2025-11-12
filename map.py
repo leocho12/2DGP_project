b0247c34706618fd4cb9d85baf769342b51d0862
leocho12 <hjc021212@gmail.com>
2025-11-12 13:03:11 +0900

    땅 파일병 변경

--- map.py
+++ map.py
@@ -3,7 +3,7 @@ from pico2d import load_image, get_canvas_width, get_canvas_height
 
 class Map:
     def __init__(self):
-        self.image = load_image('Theme_02_L130_Hill_01.png')
+        self.image = load_image('ground.png')
         self.cw=get_canvas_width()
         self.ch=get_canvas_height()
         self.w=self.cw
