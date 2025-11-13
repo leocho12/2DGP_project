43e324d70d6e506cc1ec5831c15a925101df0f6f
leocho12 <hjc021212@gmail.com>
2025-11-13 23:52:23 +0900

    풀 크기 조정

--- map.py
+++ map.py
@@ -13,31 +13,42 @@ class Map:
         self.sky_image = load_image('sky.png')
         self.sky_tiles = []
         self.sky_tile_width = self.sky_image.w
-        # grass
-        self.grass_images = [
-            load_image('grass_left.png'),
-            load_image('grass_center.png'),
-            load_image('grass_right.png')
-        ]
-        self.grass_positions = [
-            (100, 60),  # 왼쪽 잔디
-            (400, 60),  # 중앙 잔디
-            (700, 60)  # 오른쪽 잔디
-        ]
-        self.grass_scales = [
-            1.6,  # 왼쪽 잔디를 160% 크기로 표시
-            1.0,
-            1.0
-        ]
-
         # 화면을 덮을 하늘 타일 개수 계산 (여유분 포함)
         screen_width = get_canvas_width()
         tile_count = (screen_width // self.sky_tile_width) + 3
-
         # 하늘 타일 위치 초기화
         for i in range(tile_count):
             x = i * self.sky_tile_width
             self.sky_tiles.append(x)
+        # grass
+        self.grass_items = []
+        # 예시: 기존 이미지 3개를 아이템으로 추가
+        self.add_grass(load_image('grass_left.png'), 100, 60, 1.6, anchor='bottom')
+        self.add_grass(load_image('grass_center.png'), 400, 60, 1.0, anchor='bottom')
+        self.add_grass(load_image('grass_right.png'), 700, 60, 1.0, anchor='bottom')
+# API: 아이템 추가
+    def add_grass(self, image, x, y, scale=1.0, anchor='bottom'):
+        item = {'img': image, 'x': x, 'y': y, 'scale': scale, 'anchor': anchor}
+        self.grass_items.append(item)
+        return len(self.grass_items) - 1  # 추가된 인덱스 반환
+
+    # API: 위치 조정 (인덱스 사용)
+    def set_grass_position(self, index, x=None, y=None):
+        if 0 <= index < len(self.grass_items):
+            if x is not None:
+                self.grass_items[index]['x'] = x
+            if y is not None:
+                self.grass_items[index]['y'] = y
+
+    # API: 스케일 조정
+    def set_grass_scale(self, index, scale):
+        if 0 <= index < len(self.grass_items):
+            self.grass_items[index]['scale'] = scale
+
+    # API: 제거
+    def remove_grass(self, index):
+        if 0 <= index < len(self.grass_items):
+            self.grass_items.pop(index)
 
     def draw(self):
         # sky
@@ -47,17 +58,25 @@ class Map:
         # ground
         self.image.draw(self.cw//2, self.h//2, self.w, self.h)
         # grass
-        for i, (img, (x, center_y)) in enumerate(zip(self.grass_images, self.grass_positions)):
-            scale = self.grass_scales[i] if i < len(self.grass_scales) else 1.0
+        for item in self.grass_items:
+            img = item['img']
+            x = item['x']
+            y = item['y']
+            scale = item.get('scale', 1.0)
+            anchor = item.get('anchor', 'bottom')
+
             orig_w = getattr(img, 'w', 80)
             orig_h = getattr(img, 'h', 80)
 
             new_w = orig_w * scale
             new_h = orig_h * scale
 
-            # 기존 중심 기준에서 '아래쪽(바닥)' 위치를 유지하도록 중심 Y 보정
-            prev_bottom = center_y - (orig_h / 2)
-            new_center_y = prev_bottom + (new_h / 2)
+            if anchor == 'bottom':
+                # 저장된 y를 '바닥(y)'로 보고 중심 Y로 변환
+                new_center_y = y + new_h / 2.0
+            else:
+                # 'center'라면 y는 중심 Y
+                new_center_y = y
 
             img.draw(x, new_center_y, new_w, new_h)
 
