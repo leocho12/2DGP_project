06f47683de37a5fe310f14ef6a43eb3e758676f5
leocho12 <hjc021212@gmail.com>
2025-11-13 23:02:27 +0900

    화면에 풀 이미지 나타나도록 수정

--- map.py
+++ map.py
@@ -13,6 +13,17 @@ class Map:
         self.sky_image = load_image('sky.png')
         self.sky_tiles = []
         self.sky_tile_width = self.sky_image.w
+        # grass
+        self.grass_images = [
+            load_image('grass_left.png'),
+            load_image('grass_center.png'),
+            load_image('grass_right.png')
+        ]
+        self.grass_positions = [
+            (100, 60),  # 왼쪽 잔디
+            (400, 60),  # 중앙 잔디
+            (700, 60)  # 오른쪽 잔디
+        ]
 
         # 화면을 덮을 하늘 타일 개수 계산 (여유분 포함)
         screen_width = get_canvas_width()
@@ -28,6 +39,9 @@ class Map:
             self.sky_image.draw(x + self.sky_tile_width // 2,
                               get_canvas_height() - self.sky_image.h // 2)
         self.image.draw(self.cw//2, self.h//2, self.w, self.h)
+
+        for img, (x, y) in zip(self.grass_images, self.grass_positions):
+            img.draw(x, y)
 
     def update(self):
         pass
