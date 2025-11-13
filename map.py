7624b08967bfb8a6bbdde8c38c5b4362d979bfcd
leocho12 <hjc021212@gmail.com>
2025-11-13 23:21:09 +0900

    풀 크기 조정

--- map.py
+++ map.py
@@ -24,6 +24,11 @@ class Map:
             (400, 60),  # 중앙 잔디
             (700, 60)  # 오른쪽 잔디
         ]
+        self.grass_scales = [
+            1.6,  # 왼쪽 잔디를 160% 크기로 표시
+            1.0,
+            1.0
+        ]
 
         # 화면을 덮을 하늘 타일 개수 계산 (여유분 포함)
         screen_width = get_canvas_width()
@@ -35,13 +40,26 @@ class Map:
             self.sky_tiles.append(x)
 
     def draw(self):
+        # sky
         for x in self.sky_tiles:
             self.sky_image.draw(x + self.sky_tile_width // 2,
                               get_canvas_height() - self.sky_image.h // 2)
+        # ground
         self.image.draw(self.cw//2, self.h//2, self.w, self.h)
+        # grass
+        for i, (img, (x, center_y)) in enumerate(zip(self.grass_images, self.grass_positions)):
+            scale = self.grass_scales[i] if i < len(self.grass_scales) else 1.0
+            orig_w = getattr(img, 'w', 80)
+            orig_h = getattr(img, 'h', 80)
 
-        for img, (x, y) in zip(self.grass_images, self.grass_positions):
-            img.draw(x, y)
+            new_w = orig_w * scale
+            new_h = orig_h * scale
+
+            # 기존 중심 기준에서 '아래쪽(바닥)' 위치를 유지하도록 중심 Y 보정
+            prev_bottom = center_y - (orig_h / 2)
+            new_center_y = prev_bottom + (new_h / 2)
+
+            img.draw(x, new_center_y, new_w, new_h)
 
     def update(self):
         pass
