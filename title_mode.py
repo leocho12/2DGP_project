701465bc62796854dc1460a5ddee072f59d9a4f8
leocho12 <hjc021212@gmail.com>
2025-11-16 21:05:49 +0900

    타이틀 모드 와 main연동

--- title_mode.py
+++ title_mode.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import game_framework
+import play_mode
 
 image=None
 
@@ -10,16 +11,37 @@ def init():
     except Exception:
         image=None
 
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            # 스페이스바 누르면 play_mode로 전환
+            if getattr(event, 'key', None) == SDLK_SPACE:
+                game_framework.change_mode(play_mode)
+            # ESC 종료
+            elif getattr(event, 'key', None) == SDLK_ESCAPE:
+                game_framework.quit()
+
+def update():
+    pass
+
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    if image:
+        image.draw(get_canvas_width() // 2, get_canvas_height() // 2)
+    update_canvas()
+
 def finish():
     global image
-    del image
+    image = None
 
-def hande_event(event):
-    event_list=get_events()
-    for event in event_list:
-        if event.type == SDL_KEYDOWN and event.key.keysym.sym == SDLK_SPACE:
-            game_framework.change_state('play_mode')
-        elif event.type == SDL_QUIT:
-            game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key.keysym.sym == SDLK_ESCAPE:
-            game_framework.quit()
+def pause():
+    pass
+
+def resume():
+    pass
