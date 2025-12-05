e90eb9e9db33bc8d6772a19b6ce2e73ffdaf6140
leocho12 <hjc021212@gmail.com>
2025-12-05 18:52:51 +0900

    일반오리 풀보다 뒤에 그려지게 수정

--- game_world.py
+++ game_world.py
@@ -3,8 +3,8 @@ world = [[], [], [], []]  # 게임 내 객체들을 담는 리스트
 # 렌더 순서를 Background -> Grass -> Foreground -> UI 로 변경
 # (이전에는 Foreground가 1, Grass가 2라서 풀이 오리 위에 그려졌음)
 LAYER_BACKGROUND = 0    # 배경 레이어
-LAYER_GRASS = 1         # 풀 레이어 (배경 다음)
-LAYER_FOREGROUND = 2    # 오리 레이어 (풀이 뒤)
+LAYER_GRASS = 2         # 풀 레이어 (배경 다음)
+LAYER_FOREGROUND = 1    # 오리 레이어 (풀이 뒤)
 LAYER_UI = 3            # UI 레이어
 
 # 게임 통계
